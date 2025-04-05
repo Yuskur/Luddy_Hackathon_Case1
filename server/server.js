6926ee94a27f691540a044158bf4b723adc0301a
Yuskur <yisakor.melke@gmail.com>
2025-04-05 17:25:11 -0400

    .

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+dotenv.config()
 
 const app = express()
 const port = 5001
 app.use(express.json()) // This is for parsing incoming json payloads
-
 
 
 

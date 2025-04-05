f04719ff2e98dbb8e8ccae26a27b9839a4b5ecf2
Yuskur <yisakor.melke@gmail.com>
2025-04-04 23:56:58 -0400

    Docker Files

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express()
 const port = 5000
+app.use(express.json()) // This is for parsing incoming json payloads
+
+
+
 
 app.listen(port, () => {
     console.log('listening')

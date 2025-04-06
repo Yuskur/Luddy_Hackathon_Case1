e0e299287c22911722b2c1ca45f5b4ad7ccb49be
Yuskur <yisakor.melke@gmail.com>
2025-04-06 10:00:34 -0400

    .

--- client/src/MoreInfo.js
+++ client/src/MoreInfo.js
@@ -2,6 +2,22 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./MoreInfo.css";
 
-function MoreInfo(){}
+
+function MoreInfo(props) {
+    return props.trigger ? (
+        <div className="popup-body">
+            <div className="popup-inner">
+                <button className="close-button" onClick={() => props.setTrigger(false)}>
+                    x
+                </button>
+                <h2 className="popup-title">More Info</h2>
+                <div className="popup-content">
+                    This is the reasoning and action steps taken
+                </div>
+            </div>
+        </div>
+    ) : null;
+}
+
 
 export default MoreInfo;
